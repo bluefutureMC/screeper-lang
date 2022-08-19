00ae5ef6c3cd1444051bebd84a89402bc25bdc02
Noam Smadar <noam@noamsmadar.com>
2022-08-19 23:04:47 +0300

    Defined all expression posibilities in enum form

--- screeper/src/compiler/expressions.rs
+++ screeper/src/compiler/expressions.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap};
+
 use super::tokens::Token;
 
 pub enum Path {
@@ -8,25 +10,29 @@ pub enum Path {
 
 pub enum RefExpression {
     Path(Path),
-    Assign(Box<RefExpression>, Box<Expression>),
-    AddAssign(Box<RefExpression>, Box<Expression>),
-    SubtractAssign(Box<RefExpression>, Box<Expression>),
-    MultiplyAssign(Box<RefExpression>, Box<Expression>),
-    DivideAssign(Box<RefExpression>, Box<Expression>),
-    ModulusAssign(Box<RefExpression>, Box<Expression>),
-    PreIncrement(Box<RefExpression>),
-    PostIncrement(Box<RefExpression>),
-    PreDecrement(Box<RefExpression>),
-    PostDecrement(Box<RefExpression>),
-    Indexing(Box<Expression>, Box<ConstExpression>),
+    BinaryAssign(Box<RefExpression>, Token, Box<Expression>),
+    PostUniOperation(Box<RefExpression>, Token),
+    PreUniOperation(Token, Box<RefExpression>),
+    Indexing(Box<Expression>, ConstExpression),
     InnerValue(Box<Expression>, Token),
     Cast(Path, Box<Expression>)
 }
 
 pub enum ConstExpression {
-    
+    Literal(Token),
+    Object(HashMap<Token, ConstExpression>),
+    List(Vec<ConstExpression>),
+    BinaryOperation(Box<ConstExpression>, Token, Box<ConstExpression>),
+    Tuple(Box<ConstExpression>)
 }
 
 pub enum Expression {
-
+    Ref(RefExpression),
+    Object(HashMap<Token, Expression>),
+    List(Vec<Expression>),
+    Const(ConstExpression),
+    Tuple(Box<Expression>),
+    Call(RefExpression, Vec<Expression>),
+    BinaryOperation(Box<Expression>, Token, Box<Expression>),
+    Swap(RefExpression, RefExpression)
 }
