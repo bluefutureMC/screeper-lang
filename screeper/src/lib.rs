e74a49e39aa7a9be895e2543378f57ab89a7b752
Noam Smadar <noam@noamsmadar.com>
2022-08-19 13:46:47 +0300

    Added documentation

--- screeper/src/lib.rs
+++ screeper/src/lib.rs
@@ -1,11 +1,39 @@
 use std::collections::{LinkedList, HashMap};
 
+/// A structure that keeps track of values of identifiers that exist in cascading scopes
+/// 
+/// # Example
+/// ```
+/// use screeper::Scope;
+/// 
+/// let mut scope: Scope<i32> = Scope::new();
+/// 
+/// scope.push("number", 1);
+/// assert_eq!(*scope.get("number").unwrap(), 1);
+/// assert!(scope.get("bla").is_none());
+/// 
+/// scope.expand();
+/// 
+/// scope.push("number", 2);
+/// scope.push("number", 3);
+/// scope.push("bla", 100);
+/// 
+/// assert_eq!(*scope.get("number").unwrap(), 3);
+/// assert_eq!(*scope.get("bla").unwrap(), 100);
+/// 
+/// scope = scope.collapse();
+/// 
+/// assert_eq!(*scope.get("number").unwrap(), 1);
+/// assert!(scope.get("bla").is_none());
+/// ```
 pub struct Scope<T> {
     identifier_map: HashMap<String, LinkedList<(usize, T)>>,
     current_scope: usize
 }
 
 impl<T> Scope<T> {
+
+    /// Constructs a new Scope instance
     pub fn new() -> Self {
         Scope {
             identifier_map: HashMap::new(),
@@ -13,6 +41,33 @@ impl<T> Scope<T> {
         }
     }
 
+    /// Sets the given `identifier`'s value for the current scope to `value`.
+    /// When the current scope ends, this value is going to be dropped and the identifier will return to it's previous value or will be dropped completely.
+    /// 
+    /// # Example
+    /// ```
+    /// use screeper::Scope;
+    /// 
+    /// let mut scope: Scope<i32> = Scope::new();
+    /// 
+    /// scope.push("number", 1);
+    /// assert_eq!(*scope.get("number").unwrap(), 1);
+    /// assert!(scope.get("bla").is_none());
+    /// 
+    /// scope.expand();
+    /// 
+    /// scope.push("number", 2);
+    /// scope.push("number", 3);
+    /// scope.push("bla", 100);
+    /// 
+    /// assert_eq!(*scope.get("number").unwrap(), 3);
+    /// assert_eq!(*scope.get("bla").unwrap(), 100);
+    /// 
+    /// scope = scope.collapse();
+    /// 
+    /// assert_eq!(*scope.get("number").unwrap(), 1);
+    /// assert!(scope.get("bla").is_none());
+    /// ```
     pub fn push(&mut self, identifier: &str, value: T) {
 
         if let Some(value_stack) = self.identifier_map.get_mut(identifier) {
@@ -29,21 +84,117 @@ impl<T> Scope<T> {
 
     }
 
+    /// Gets the given `identifier`'s value for the current scope.
+    /// 
+    /// # Example
+    /// ```
+    /// use screeper::Scope;
+    /// 
+    /// let mut scope: Scope<i32> = Scope::new();
+    /// 
+    /// scope.push("number", 1);
+    /// assert_eq!(*scope.get("number").unwrap(), 1);
+    /// assert!(scope.get("bla").is_none());
+    /// 
+    /// scope.expand();
+    /// 
+    /// scope.push("number", 2);
+    /// scope.push("number", 3);
+    /// scope.push("bla", 100);
+    /// 
+    /// assert_eq!(*scope.get("number").unwrap(), 3);
+    /// assert_eq!(*scope.get("bla").unwrap(), 100);
+    /// 
+    /// scope = scope.collapse();
+    /// 
+    /// assert_eq!(*scope.get("number").unwrap(), 1);
+    /// assert!(scope.get("bla").is_none());
+    /// ```
     pub fn get<'a>(&'a self, key: &str) -> Option<&'a T> {
         Some(&self.identifier_map.get(key)?.front()?.1)
     }
 
+    /// Expands the current scope. Any values pushed after this method is executed will be dropped after the corrosponding collapse.
+    /// 
+    /// # Example
+    /// ```
+    /// use screeper::Scope;
+    /// 
+    /// let mut scope: Scope<i32> = Scope::new();
+    /// 
+    /// scope.push("num", 1);
+    /// 
+    /// scope.expand();
+    /// 
+    /// scope.push("num", 2);
+    /// scope.push("num", 3);
+    /// 
+    /// assert_eq!(*scope.get("num").unwrap(), 3);
+    /// 
+    /// scope.expand();
+    /// 
+    /// scope.push("num", 4);
+    /// 
+    /// assert_eq!(*scope.get("num").unwrap(), 4);
+    /// 
+    /// scope = scope.collapse();
+    /// 
+    /// assert_eq!(*scope.get("num").unwrap(), 3);
+    /// 
+    /// scope = scope.collapse();
+    /// 
+    /// assert_eq!(*scope.get("num").unwrap(), 1);
+    /// ```
     pub fn expand(&mut self) {
         self.current_scope += 1;
     }
 
-    pub fn collapse(&mut self) {
+    /// Collapses the current scope. Any values pushed during the current scope will be reverted to their state in the previous scope.
+    /// This method consumes the scope instance and returns a new one to force the lifetime of any references to previous values to end.
+    /// 
+    /// # Example
+    /// ```
+    /// use screeper::Scope;
+    /// 
+    /// let mut scope: Scope<i32> = Scope::new();
+    /// 
+    /// scope.push("num", 1);
+    /// 
+    /// scope.expand();
+    /// 
+    /// scope.push("num", 2);
+    /// scope.push("num", 3);
+    /// 
+    /// assert_eq!(*scope.get("num").unwrap(), 3);
+    /// 
+    /// scope.expand();
+    /// 
+    /// scope.push("num", 4);
+    /// 
+    /// let current_num: &i32 = scope.get("num").unwrap();
+    /// 
+    /// assert_eq!(*current_num, 4);
+    /// 
+    /// scope = scope.collapse();
+    /// 
+    /// // assert_eq!(*current_num, 3); X
+    /// // wont compile because current_num is no longer a valid reference
+    /// 
+    /// assert_eq!(*scope.get("num").unwrap(), 3);
+    /// 
+    /// scope = scope.collapse();
+    /// 
+    /// assert_eq!(*scope.get("num").unwrap(), 1);
+    /// ```
+    pub fn collapse(mut self) -> Self {
         self.current_scope -= 1;
         for value_stack in self.identifier_map.values_mut() {
             if match value_stack.front() { Some(front) => front.0, None => 0 } > self.current_scope {
                 value_stack.pop_front();
             }
         }
+
+        self
     }
 }
 
@@ -58,8 +209,8 @@ mod tests {
         scope.push("num1", 1);
         scope.push("num2", 2);
         
-        assert_eq!(*scope.get("num1").unwrap(), 1i32);
-        assert_eq!(*scope.get("num2").unwrap(), 2i32);
+        assert_eq!(*scope.get("num1").unwrap(), 1);
+        assert_eq!(*scope.get("num2").unwrap(), 2);
         assert!(scope.get("num3").is_none());
 
         scope.expand();
@@ -67,14 +218,14 @@ mod tests {
         scope.push("num3", 3);
         scope.push("num1", 4);
 
-        assert_eq!(*scope.get("num1").unwrap(), 4i32);
-        assert_eq!(*scope.get("num2").unwrap(), 2i32);
-        assert_eq!(*scope.get("num3").unwrap(), 3i32);
+        assert_eq!(*scope.get("num1").unwrap(), 4);
+        assert_eq!(*scope.get("num2").unwrap(), 2);
+        assert_eq!(*scope.get("num3").unwrap(), 3);
 
-        scope.collapse();
+        scope = scope.collapse();
 
-        assert_eq!(*scope.get("num1").unwrap(), 1i32);
-        assert_eq!(*scope.get("num2").unwrap(), 2i32);
+        assert_eq!(*scope.get("num1").unwrap(), 1);
+        assert_eq!(*scope.get("num2").unwrap(), 2);
         assert!(scope.get("num3").is_none());
 
     }
